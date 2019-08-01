4b7a9735b669179b3402bfd3c4b7b601be87af33
Nigel <neastern101@yahoo.com>
2019-08-01 15:11:17 -0500

    add mongodb config

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,4 +1,12 @@
+var mongoose = require("mongoose");
+const hashpass = require('hashpass');
+const uuidv1 = require('uuid');
+require("dotenv").config();
+
 const db = require("../models");
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/renter-app";
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 // Defining methods for the usersController
 module.exports = {
@@ -13,5 +21,8 @@ module.exports = {
     logout: (request, response) => {
         // request.logout();
         response.send("user logged out");
+    },
+    create: (request, response) => {
+        response.json("return created user")
     }
 };
