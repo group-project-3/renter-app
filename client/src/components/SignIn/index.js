6ff76406785eaa368de12779ee6e97bca8cee8d7
Mike Gluszek <mgluszek710@gmail.com>
2019-08-01 20:59:30 -0500

    Create index.js

---
+++ client/src/components/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function SignIn() {
+    return (
+        <div className="sign-in">
+            
+        </div>
+    );
+}
+
+export default SignIn;
