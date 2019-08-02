ed7cf48d7bda1a6c3ac79882d63d3c57a5465894
Mike Gluszek <mgluszek710@gmail.com>
2019-08-01 20:59:35 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
-// import logo from './logo.svg';
-// import './App.css';
 
 function App() {
   return (
