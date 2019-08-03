8dd678b46eaebdcccde96ffdbebcd3f5653aad00
alexcahn <alexbcahn@gmail.com>
2019-08-03 18:25:37 -0500

    Update LogIn.js

--- client/src/pages/LogIn/LogIn.js
+++ client/src/pages/LogIn/LogIn.js
@@ -1,9 +1,10 @@
 import React from "react";
+import API from "../../utils/API"
 
 export default function LogIn() {
     return (
-     <div>
-         <h1>Log In Page</h1>
-     </div>
+        <div>
+            <h1>Log In Page</h1>
+        </div>
     );
-  }
+}
