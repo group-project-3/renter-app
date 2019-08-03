04583611e5c16b83ee4fd9b65b331b7012070667
Mike Gluszek <mgluszek710@gmail.com>
2019-08-03 10:30:04 -0500

    sign-in page

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {Components} from "react";
 import "./style.css";
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+
+
+
 
 function SignIn() {
     return (
-        <div className="sign-in">
-            
-        </div>
+  
+<div className="sign-in">
+
+<Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="normal" placeholder="Enter Username" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  
+  <Button variant="primary" type="submit">
+    Sign-In
+  </Button>
+</Form>
+
+</div>
+  
     );
 }
 
