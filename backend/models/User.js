c4acf63813f8083e28abe52ab3068b25c816a460
alexcahn <alexbcahn@gmail.com>
2019-08-03 12:13:22 -0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email_address: { type: String, required: true },
+    first_name: { type: String },
+    last_name: { type: String },
+    email_address: { type: String },
+    username: { type: String },
     role: { type: String },
-    password: { type: String, required: true },
+    password: { type: String },
     salt: { type: String },
     session_token: { type: String },
     created: { type: Date, default: Date.now },
