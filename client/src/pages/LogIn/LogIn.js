ea7d35347000a2f9af32dc5210aacceea6c7a693
Mike Gluszek <mgluszek710@gmail.com>
2019-08-03 11:44:48 -0500

    Update LogIn.js

--- client/src/pages/LogIn/LogIn.js
+++ client/src/pages/LogIn/LogIn.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { Components } from "react";
+import { Col, Row, Container } from "../../components/Grid/Grid";
+import { Input, PasswordInput, FormBtn } from "../../components/Form/Form"
 
-export default function LogIn() {
-    return (
-     <div>
-         <h1>Log In Page</h1>
-     </div>
-    );
-  }
+class LogIn extends Component {
+    state = {
+        username: "",
+        password: ""
+    };
+
+
+    render() {
+        return (
+            <div>
+                <Container fluid>
+                    <Row>
+                        <Col size="md-3"></Col>
+                        <Col size="md-6">
+                            <h1>Log-In</h1>
+                            <br></br>
+                            <form>
+                                <Input
+                                    value={this.state.username}
+                                    onChange={this.handleInputChange}
+                                    name="username"
+                                    placeholder="Enter Username"
+                                />
+
+                                <PasswordInput
+                                    value={this.state.password}
+                                    onChange={this.handleInputChange}
+                                    name="password"
+                                    placeholder="Enter Password"
+                                />
+
+                                <FormBtn onClick={this.handleFormSubmit}>
+                                    Submit
+                                </FormBtn>
+                            </form>
+                        </Col>
+                        <Col size="md-3"></Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+
+}
+
+
+export default LogIn;
