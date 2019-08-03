6ef3a5d26d2ff3a36ea9eb43ce1a549e32da16b4
Mike Gluszek <mgluszek710@gmail.com>
2019-08-03 10:30:26 -0500

    import sign-in

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Jumbotron />
+      <SignIn />
     </div>
   );
 }
