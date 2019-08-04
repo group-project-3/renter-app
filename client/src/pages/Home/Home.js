e2403b4294ce85d576ad8c644cdce8aa4d6decb6
Anthony Gaimari <tgaimari@comcast.net>
2019-08-04 13:07:49 -0500

    Card setup
    Tseting card setup

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import "../../components/Card/Card.js";
+import { Container, Row, Col} from "../../components/Grid/Grid.js"
 
-export default function Home() {
-    return (
-      <div>
-          <h1>THIS IS THE HOME PAGE</h1>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-    );
+// export default function Home() {
+//     return (
+//       <div>
+//           <h1>THIS IS THE HOME PAGE</h1>
+//            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+//         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+//         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+//         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+//       </div>
+//     );
+//   }
+class Card extends Component {
+  // state = {
+  //   item_name: "",
+  //   url: "",
+  //   location_id: {},
+  //   owner_id: {
+  //     type: Schema.Types.ObjectId,
+  //     ref: "User"
+  //   },
+  //   price: { type: Number },
+  //   available: { type: Boolean }
+  // };
+  render() {
+      return (
+          <div>
+              <Container fluid>
+                  <Row>                      
+                      <Col size="md-3">
+                          <h1>Item Name</h1>
+                          <br></br>
+                      </Col>                     
+                  </Row>
+              </Container>
+          </div>
+      )
   }
+
+}
+
+
+export default Card;
