d1e45f3f467a854cd927fbf98d183977f96780e5
Mike Gluszek <mgluszek710@gmail.com>
2019-08-08 20:35:13 -0500

    Redirecting login page to home

--- client/src/pages/LogIn/LogIn.js
+++ client/src/pages/LogIn/LogIn.js
@@ -1,69 +1,80 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid/Grid";
 import { Input, PasswordInput, FormBtn } from "../../components/Form/Form"
+import { BrowserRouter as Router, Redirect, Switch } from "react-router-dom";
 import API from "../../utils/API";
 import "./style.css";
 
 class LogIn extends Component {
     state = {
         username: "",
-        password: ""
+        password: "",
+        redirect: false
     };
 
     handleInputChange = event => {
         const { name, value } = event.target;
-        this.setState({[name]: value});
-      };
+        this.setState({ [name]: value });
+    };
 
     handleFormSubmit = event => {
         event.preventDefault();
-      let loginObj = {
-          username: this.state.username, 
-          password: this.state.password
-      }
-      
-            API.login(loginObj)
+        let loginObj = {
+            username: this.state.username,
+            password: this.state.password
+        }
+
+        API.login(loginObj)
             .then(res => console.log(res))
             .catch(err => console.log(err));
-             
+
+        this.setState({ redirect: true })
+
     }
 
     render() {
+        if (this.state.redirect === true) {
+            return <Redirect to='/' />
+        }
         return (
-            <div>
-                <Container fluid>
-                    <Row>
-                        <Col size="md-3"></Col>
-                        <Col size="md-6">
-                            <h1>Log-In</h1>
-                            <br></br>
-                            <form>
-                                <p>Username</p>
-                                <Input
-                                    value={this.state.username}
-                                    onChange={this.handleInputChange}
-                                    name="username"
-                                    placeholder="Enter Your Username"
-                                />
+            <Router>
+                <Switch>
+                    <div>
+                        <Container fluid>
+                            <Row>
+                                <Col size="md-3"></Col>
+                                <Col size="md-6">
+                                    <h1>Log-In</h1>
+                                    <br></br>
+                                    <form>
+                                        <p>Username</p>
+                                        <Input
+                                            value={this.state.username}
+                                            onChange={this.handleInputChange}
+                                            name="username"
+                                            placeholder="Enter Your Username"
+                                        />
 
-                                <p>Password</p>
-                                <PasswordInput
-                                    value={this.state.password}
-                                    onChange={this.handleInputChange}
-                                    name="password"
-                                    placeholder="Enter Your Password"
-                                />
+                                        <p>Password</p>
+                                        <PasswordInput
+                                            value={this.state.password}
+                                            onChange={this.handleInputChange}
+                                            name="password"
+                                            placeholder="Enter Your Password"
+                                        />
+                                        <FormBtn onClick={this.handleFormSubmit} >
+                                            Log In
+                                        </FormBtn>
 
-                                <FormBtn onClick={this.handleFormSubmit}>
-                                    Log In
-                                </FormBtn>
-                            </form>
-                        </Col>
-                        <Col size="md-3"></Col>
-                    </Row>
-                </Container>
-            </div>
-        )
+                                    </form>
+                                </Col>
+                                <Col size="md-3"></Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </Switch>
+            </Router>
+        );
     }
 
 }
