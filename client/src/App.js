a4661f9a50377dbd7edad587ee8799e2fabcf2e3
alexcahn <alexbcahn@gmail.com>
2019-08-13 21:07:39 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from './components/Nav/Nav';
 import Jumbotron from './components/Jumbotron/Jumbotron';
@@ -7,29 +7,44 @@ import Register from "./pages/Register/Register";
 import LogIn from "./pages/LogIn/LogIn"
 import PostItem from './pages/PostItem/PostItem';
 import RentItem from './pages/RentItem/RentItem';
-import AccountSettings from './pages/AccountSettings/AccountSettings';
 import DatePick from './components/DatePick/DatePick';
+import { UserProvider } from './UserContext'
+import API from './utils/API'
 
+class App extends Component {
 
-function App() {
-  return (
+  state = {
+    user: {}
+  }
 
+  componentDidMount() {
+    let user_session = localStorage.getItem("x-session-token")
+    if (user_session) {
+      API.getAuthenticatedUser(user_session)
+        .then(userObject => {
+          this.setState({ user: userObject.data })
+        })
+    } else {
+      console.log('user not logged in')
+    }
+  }
+
+  render() {
+    return (
       <Router>
-      <Nav />
-      <Jumbotron />
-
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/datepick" component={DatePick} />
-        <Route exact path="/PostItem" component={PostItem} />
-        <Route exact path="/RentItem" component={RentItem} />
-        <Route exact path="/LogIn" component={LogIn} />
-        <Route exact path="/Register" component={Register} />
-        <Route exact path="/AccountSettings" component={AccountSettings} />
-      </Switch>
+        <UserProvider value={this.state.user}>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/datepick" component={DatePick} />
+            <Route exact path="/PostItem" component={PostItem} />
+            <Route exact path="/RentItem" component={RentItem} />
+            <Route exact path="/LogIn" component={LogIn} />
+            <Route exact path="/Register" component={Register} />
+          </Switch>
+        </UserProvider>
       </Router>
-
-  );
+    );
+  }
 }
-
 export default App;
