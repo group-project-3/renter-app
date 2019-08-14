200b002189099812fcb870fbdad3338149aae4fd
alexcahn <alexbcahn@gmail.com>
2019-08-13 21:07:33 -0500

    Update ItemRented.js

--- backend/models/ItemRented.js
+++ backend/models/ItemRented.js
@@ -10,10 +10,10 @@ const itemRentedSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User"
     },
-    rented_from: { type: Date },
-    rented_to: { type: Date },
-    returned: { type: Date },
-    price: { type: Number }
+    rented_from: { type: Date, required: true },
+    rented_to: { type: Date, required: true },
+    returned: { type: Date, required: true },
+    price: { type: Number, required: true }
 });
 const ItemRented = mongoose.model("ItemRented", itemRentedSchema);
 
