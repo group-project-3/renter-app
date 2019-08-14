096cd50f70ce384c5e7ee56584bc3891ffd900b7
Nigel <neastern101@yahoo.com>
2019-08-13 20:13:53 -0500

    moved mongodb connection to server.js

--- backend/controllers/itemsRentedController.js
+++ backend/controllers/itemsRentedController.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const db = require("../models");
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/renter-app";
-mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+// const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/renter-app";
+// mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 module.exports = {
     findAll: (request, response) => {
