b3e74dfac8afba30952cf2865e5e1b0f3850ec40
alexcahn <alexbcahn@gmail.com>
2019-08-15 20:55:21 -0500

    Update Card.js

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -6,30 +6,30 @@ import "./style.css";
 
 export function Card(props) {
     return (
-        <div className="card" style={{"min-width" : "960px", "max-height" : "200px", "margin-bottom" : "50px", "margin-left" : "200px"}}>
+        <div className="card" style={{ "min-width": "960px", "max-height": "200px", "margin-bottom": "50px", "margin-left": "200px" }}>
             <Row>
-            <Col size="md-6" className="itemImg" style={{"max-height" : "185px", "float" : "left"}}>
-                <img src={props.url} className="card-img-top" alt="..." style={{"max-height" : "300px", "float" : "left"}}></img>
-            </Col>
-            <Col size="md-6" className="itemInfo" style={{"float" : "right"}}>
-                <div className="card-body">
-                    <h5 className="card-title">{props.name}</h5>
-                    <p className="">{props.description}</p>
-                    <p className="">{props.price}</p>
-                    <p>Rent Duration:</p>
-                    <DatePick {...props} />
-                    {props.available ?
-                    
-                        <button className="btn btn-sm  btn-primary"
-                            onClick={() => { props.handleRentItem(props.id) }}>Rent
+                <Col size="md-6" className="itemImg" style={{ "max-height": "185px", "float": "left" }}>
+                    <img src={props.url} className="card-img-top" alt="..." style={{ "max-height": "300px", "float": "left" }}></img>
+                </Col>
+                <Col size="md-6" className="itemInfo" style={{ "float": "right" }}>
+                    <div className="card-body">
+                        <h5 className="card-title">{props.name}</h5>
+                        <p className="">{props.description}</p>
+                        <p className="">{props.price}</p>
+                        <p>Rent Duration:</p>
+                        <DatePick {...props} />
+                        {props.available ?
+
+                            <button className="btn btn-sm  btn-primary"
+                                onClick={() => { props.handleRentItem(props.id) }}>Rent
                     </button>
-                        :
-                        <button className="btn btn-sm btn-primary"
-                            onClick={() => { props.handleReturnItem(props.id) }}>Return
+                            :
+                            <button className="btn btn-sm btn-primary"
+                                onClick={() => { props.handleReturnItem(props.id) }}>Return
                     </button>
-                    }
-                </div>
-            </Col>
+                        }
+                    </div>
+                </Col>
             </Row>
         </div>
 
