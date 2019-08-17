a303dfc89ee9438d94872f6cb0248fbfa29e8129
alexcahn <alexbcahn@gmail.com>
2019-08-17 11:37:46 -0500

    Update Jumbotron.js

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./style.css";
+import { MDBCol, MDBIcon } from "mdbreact";
 
 function Jumbotron() {
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log("click")
+    }
+
     return (
         <div className="jumbotron text-center">
-            <h1>Group Project 3</h1>
+            <h4>We got the stuff you need!</h4>
+            <div>
+                <MDBCol md="6">
+                    <form className="form-inline mt-4 mb-4">
+                        <MDBIcon icon="search" />
+                        <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+                        <button onClick={handleClick}>Search</button>
+                    </form>
+                </MDBCol>
+            </div>
         </div>
     );
 }
