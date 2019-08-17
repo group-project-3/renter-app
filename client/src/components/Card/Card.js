10aaeda1b810cfa62ac652730612c6957ff2db82
Anthony Gaimari <tgaimari@comcast.net>
2019-08-17 11:31:31 -0500

    Btn Align bottom

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -9,27 +9,29 @@ export function Card(props) {
         <div className="card" style={{ "min-width": "960px", "max-height": "200px", "margin-bottom": "50px", "margin-left": "200px" }}>
             <Row>
 
-            <Col size="md-6" className="itemImg" style={{"max-height" : "185px", "float" : "left"}}>
-                <img src={props.url} className="card-img-top" alt="..." style={{"max-height" : "300px", "float" : "left"}}></img>
-            </Col>
-            <Col size="md-6" className="itemInfo" style={{"float" : "right"}}>
-                <div className="card-body">
-                    <h5 className="card-title">{props.name}</h5>
-                    <p className="line">{props.description}</p>
-                    <p className=""><span>&#36;</span>{props.price}</p>
-                    <p>Rent Duration:</p>
-                    <DatePick {...props} />
-                    {props.available ?
-                    
-                        <button className="btn btn-primary btn-sm btn-bottom"
-                            onClick={() => { props.handleRentItem(props.id) }}>Rent
-
-                    </button>
+                <Col size="md-6" className="itemImg" style={{ "max-height": "185px", "float": "left" }}>
+                    <img src={props.url} className="card-img-top" alt="..." style={{ "max-height": "300px", "float": "left" }}></img>
+                </Col>
+                <Col size="md-6" className="itemInfo" style={{ "float": "right" }}>
+                    <div className="card-body">
+                        <h5 className="card-title">{props.name}</h5>
+                        <p className="line">{props.description}</p>
+                        <p className=""><span>&#36;</span>{props.price}</p>
+                        <p>Rent Duration:</p>
+                        <DatePick {...props} />
+                        <div class="card-footer">
+                        {props.available ?
+                            
+                            <button className="btn btn-primary btn-sm btn-bottom"
+                                onClick={() => { props.handleRentItem(props.id) }}>Rent
+                            </button>
                             :
                             <button className="btn btn-primary btn-sm btn-bottom"
                                 onClick={() => { props.handleReturnItem(props.id) }}>Return
-                    </button>
+                            </button>
+                            
                         }
+                        </div>
                     </div>
                 </Col>
             </Row>
