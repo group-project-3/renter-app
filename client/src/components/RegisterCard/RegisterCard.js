221c7c4be9a740b7c0efb4b39660e7784a1a4551
bee25141 <bee25141@gmail.com>
2019-08-20 19:32:00 -0500

    styled register card

--- client/src/components/RegisterCard/RegisterCard.js
+++ client/src/components/RegisterCard/RegisterCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Input, EmailInput, PasswordInput, FormBtn, LocationInput } from "../../components/Form/Form";
+import "./style.css"
 
 export default function(props) {
 
     return (
-        <div className="card-background">
+        <div className="register-background">
                                 <h1>Register a New Account</h1>
                                 <br></br>
                                 <form>
