27d850c9c9bdb7e6dabe78cde36f3fb436cf01a9
alexcahn <alexbcahn@gmail.com>
2019-08-20 19:47:17 -0500

    Update UploadImage.js

--- client/src/components/UploadImage/UploadImage.js
+++ client/src/components/UploadImage/UploadImage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import './style.css'
 
 export default function UploadImage(props) {
 
   return (
     <div>
-        <center>
-          <h5>UPLOAD AN IMAGE</h5>
-          <input onChange={props.handleImageChange} name="image" value={props.image} type="file"/>
-          <br />
-        </center>
-      </div>
+      <center>
+        <h5>UPLOAD AN IMAGE</h5>
+        <input onChange={props.handleImageChange} name="image" value={props.image} type="file" className="file" />
+        <br />
+      </center>
+    </div>
   )
 }
